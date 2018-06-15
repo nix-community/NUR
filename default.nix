b549e5a575da4129ed4ff94c39fcee619a9e5cdc
Jörg Thalheim <joerg@thalheim.io>
2018-06-15 21:26:57 +0100

    first update test

--- default.nix
+++ default.nix
@@ -7,8 +7,8 @@ in {
     mic92 = callPackages (fetchFromGitHub {
       owner = "Mic92";
       repo = "nur-packages";
-      sha256 = "0j18d3hfsy00kf2y8xp4w7bkv3wkj96vxd8pajcqqykysva44y9g";
-      rev = "bcfe13fe3767f7cbd9821f7bb37a1b225d768159";
+      sha256 = "1pybc9xk415wvw6hhmqs9wwb28g9h2500lv2xj66cjn7sww4adxq";
+      rev = "5a16e73333e14b4cb3dbbb1a2e6859f11143b3f2";
     }) {};
   };
 }
