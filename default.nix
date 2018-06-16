bbe70d3ba7907bf6036cac0191b316713b855d25
Jörg Thalheim <joerg@thalheim.io>
2018-06-16 01:51:00 +0100

    yet another repo update

--- default.nix
+++ default.nix
@@ -7,8 +7,8 @@ in {
     mic92 = callPackages (fetchFromGitHub {
       owner = "Mic92";
       repo = "nur-packages";
-      sha256 = "1pybc9xk415wvw6hhmqs9wwb28g9h2500lv2xj66cjn7sww4adxq";
-      rev = "5a16e73333e14b4cb3dbbb1a2e6859f11143b3f2";
+      sha256 = "1sk41q80z6rzrnnzbpkj9jmr9qcsxvh92q7v1jl60f5ms4q0ipx2";
+      rev = "8d61d40bf8e17555e81eeabfa7e5d4ac6e01ac37";
     }) {};
   };
 }
