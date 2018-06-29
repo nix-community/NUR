259973f0cc3d10107d8c2e6084b9f8caee67eea4
Jörg Thalheim <joerg@thalheim.io>
2018-06-29 12:55:01 +0100

    travis: correct python version

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -3,9 +3,7 @@
 set -eux -o pipefail # Exit with nonzero exit code if anything fails
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-if [[ "$TRAVIS_PULL_REQUEST" != "false" ]] || \
-	[[ "$TRAVIS_BRANCH" != master ]] && \
-	[[ "$TRAVIS_BRANCH" != "$(cat .version)" ]]; then
+if [[ "$TRAVIS_PULL_REQUEST" != "false" ]] || [[ "$TRAVIS_BRANCH" != master ]]; then
     echo "Skipping deploy; just doing a build."
     python ./nur/update.py
     nix-build
@@ -15,17 +13,15 @@ fi
 python ./nur/update.py
 nix-build
 
-if [ "$TRAVIS_BRANCH" = master ]; then
-    git config user.name "Travis CI"
-    git config user.email "$COMMIT_AUTHOR_EMAIL"
-    
-    if [ -z "$(git diff --exit-code)" ]; then
-    	echo "No changes to the output on this push; exiting."
-    	exit 0
-    fi
-    
-    git add --all repos.json*
+git config user.name "Travis CI"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
 
-    git commit -m "automatic update"
-    git push origin master
+if [ -z "$(git diff --exit-code)" ]; then
+  echo "No changes to the output on this push; exiting."
+  exit 0
 fi
+
+git add --all repos.json*
+
+git commit -m "automatic update"
+git push origin master
