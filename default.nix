97ff96075ba3a82fe55a16fc8b1966add40359bb
Jörg Thalheim <joerg@thalheim.io>
2018-08-04 17:55:52 +0100

    move repoSource to external file

--- default.nix
+++ default.nix
@@ -3,51 +3,21 @@
 , pkgs ? null }:
 
 let
-  inherit (nurpkgs) fetchgit fetchzip lib;
-
   manifest = (builtins.fromJSON (builtins.readFile ./repos.json)).repos;
   lockedRevisions = (builtins.fromJSON (builtins.readFile ./repos.json.lock)).repos;
 
-  parseGitlabUrl = url:
-    with builtins;
-    let
-      parts = lib.splitString "/" url;
-      len = length parts;
-    in {
-      domain = elemAt parts 2;
-      owner = elemAt parts (len - 2);
-      repo = elemAt parts (len - 1);
-    };
+  inherit (nurpkgs) lib;
 
-  repoSource = name: attr:
-    let
-      revision = lockedRevisions.${name};
-      submodules = attr.submodules or false;
-      type = attr.type or null;
-    in if lib.hasPrefix "https://github.com" attr.url && !submodules then
-      fetchzip {
-        url = "${attr.url}/archive/${revision.rev}.zip";
-        inherit (revision) sha256;
-      }
-    else if (lib.hasPrefix "https://gitlab.com" attr.url || type == "gitlab") && !submodules then
-      let
-        gitlab = parseGitlabUrl attr.url;
-      in fetchzip {
-        url = "https://${gitlab.domain}/api/v4/projects/${gitlab.owner}%2F${gitlab.repo}/repository/archive.tar.gz?sha=${revision.rev}";
-        inherit (revision) sha256;
-      }
-    else
-      fetchgit {
-        inherit (attr) url;
-        inherit (revision) rev sha256;
-        fetchSubmodules = submodules;
-      };
+  repoSource = name: attr: import ./lib/repoSource.nix {
+    inherit name attr manifest lockedRevisions lib;
+    inherit (nurpkgs) fetchgit fetchzip;
+  };
 
-   createRepo = name: attr: import ./lib/evalRepo.nix {
-     inherit name pkgs lib;
-     inherit (attr) url;
-     src = repoSource name attr + "/" + (attr.file or "");
-   };
+  createRepo = name: attr: import ./lib/evalRepo.nix {
+    inherit name pkgs lib;
+    inherit (attr) url;
+    src = repoSource name attr + "/" + (attr.file or "");
+  };
 
 in {
   repos =  lib.mapAttrs createRepo manifest;
---
+++ lib/repoSource.nix
@@ -0,0 +1,38 @@
+{
+  name, attr,
+  fetchgit, fetchzip, lib,
+  manifest, lockedRevisions
+}:
+
+let
+  parseGitlabUrl = url: with builtins; let
+    parts = lib.splitString "/" url;
+    len = length parts;
+  in {
+    domain = elemAt parts 2;
+    owner = elemAt parts (len - 2);
+    repo = elemAt parts (len - 1);
+  };
+
+  revision = lockedRevisions.${name};
+  submodules = attr.submodules or false;
+  type = attr.type or null;
+in
+  if lib.hasPrefix "https://github.com" attr.url && !submodules then
+    fetchzip {
+      url = "${attr.url}/archive/${revision.rev}.zip";
+      inherit (revision) sha256;
+    }
+  else if (lib.hasPrefix "https://gitlab.com" attr.url || type == "gitlab") && !submodules then
+    let
+      gitlab = parseGitlabUrl attr.url;
+    in fetchzip {
+      url = "https://${gitlab.domain}/api/v4/projects/${gitlab.owner}%2F${gitlab.repo}/repository/archive.tar.gz?sha=${revision.rev}";
+      inherit (revision) sha256;
+    }
+  else
+    fetchgit {
+      inherit (attr) url;
+      inherit (revision) rev sha256;
+      fetchSubmodules = submodules;
+    }
