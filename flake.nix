4dd06839632e586b5dfebf01f6eea911f7463405
Jörg Thalheim <joerg@thalheim.io>
2020-07-06 11:10:08 +0100

    fix flake.nix usage
    Maybe the schema was changed but assigning nixpkgs in the current no longer works.

--- flake.nix
+++ flake.nix
@@ -1,13 +1,12 @@
 {
   description = "Nix User Repository";
 
-  outputs = { self, nixpkgs }:
-    {
-      overlay = final: prev: {
-        nur = import ./default.nix {
-          nurpkgs = nixpkgs;
-          pkgs = nixpkgs.pkgs;
-        };
+  outputs = { self }: {
+    overlay = final: prev: {
+      nur = import ./default.nix {
+        nurpkgs = prev;
+        pkgs = prev;
       };
     };
+  };
 }
