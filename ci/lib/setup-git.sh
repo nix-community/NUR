c70e1e8bcf61d638c0e6c23a6f539ec7e05d8de9
Gavin John <gavinnjohn@gmail.com>
2025-02-10 13:25:23 -0800

    Switch to Github Application for Nur a bot

--- ci/lib/setup-git.sh
+++ ci/lib/setup-git.sh
@@ -2,6 +2,6 @@
 # ==================================================
 
 export GIT_AUTHOR_NAME="Nur a bot"
-export GIT_AUTHOR_EMAIL="nixpkgs-review@example.com"
+export GIT_AUTHOR_EMAIL="198656834+nur-a-bot[bot]@users.noreply.github.com"
 export GIT_COMMITTER_NAME=$GIT_AUTHOR_NAME
 export GIT_COMMITTER_EMAIL=$GIT_AUTHOR_EMAIL
