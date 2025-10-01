e5996a324a35e040241c9219e048a154175c2040
Gavin John <gavinnjohn@gmail.com>
2025-10-01 16:04:58 -0700

    Should fix https://github.com/nix-community/nur-combined/issues/41

--- ci/lib/setup-git.sh
+++ ci/lib/setup-git.sh
@@ -5,3 +5,5 @@ export GIT_AUTHOR_NAME="Nur a bot"
 export GIT_AUTHOR_EMAIL="198656834+nur-a-bot[bot]@users.noreply.github.com"
 export GIT_COMMITTER_NAME=$GIT_AUTHOR_NAME
 export GIT_COMMITTER_EMAIL=$GIT_AUTHOR_EMAIL
+
+git config --global "lfs.<unknown>.locksverify" false
