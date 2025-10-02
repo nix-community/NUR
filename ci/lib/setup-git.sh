621c01330499e0ec6580a7ef3a56dc8884c1c335
Gavin John <gavinnjohn@gmail.com>
2025-10-02 09:35:29 -0700

    Globally disable LFS locking

--- ci/lib/setup-git.sh
+++ ci/lib/setup-git.sh
@@ -6,4 +6,4 @@ export GIT_AUTHOR_EMAIL="198656834+nur-a-bot[bot]@users.noreply.github.com"
 export GIT_COMMITTER_NAME=$GIT_AUTHOR_NAME
 export GIT_COMMITTER_EMAIL=$GIT_AUTHOR_EMAIL
 
-git config --global "lfs.<unknown>.locksverify" false
+git config --global lfs.locksverify false
