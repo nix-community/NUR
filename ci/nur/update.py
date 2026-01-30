542b6747ccd38f0a60f17358fe8c1be3253f24f5
Gavin John <gavinnjohn@gmail.com>
2026-01-29 22:18:01 -0800

    Another missing import

--- ci/nur/update.py
+++ ci/nur/update.py
@@ -1,6 +1,6 @@
 import logging
 import asyncio
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 from argparse import Namespace
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
