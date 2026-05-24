697b728a226b271948def192d2f6923f6147f1ab
Gavin John <gavinnjohn@gmail.com>
2026-05-24 12:32:49 -0700

    Update git author email to point to new nur a bot app

--- ci/lib/setup-git.sh
+++ ci/lib/setup-git.sh
@@ -2,7 +2,7 @@
 # ==================================================
 
 export GIT_AUTHOR_NAME="Nur a bot"
-export GIT_AUTHOR_EMAIL="198656834+nur-a-bot[bot]@users.noreply.github.com"
+export GIT_AUTHOR_EMAIL="287493544+nur-a-bot[bot]@users.noreply.github.com"
 export GIT_COMMITTER_NAME=$GIT_AUTHOR_NAME
 export GIT_COMMITTER_EMAIL=$GIT_AUTHOR_EMAIL
 
